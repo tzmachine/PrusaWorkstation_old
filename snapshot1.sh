bb1cf17731c364f126f247c6245aa01bfbc63ae0
tzmachine <thorben.zimmermann.tz@outlook.de>
2024-06-29 13:19:55 +0200

    Backup changes Sat 29 Jun 13:19:54 CEST 2024

--- snapshot1.sh
+++ snapshot1.sh
@@ -5,7 +5,7 @@ temp=$(vcgencmd measure_temp | cut -d "=" -f2)
 token="t9hamOfw7SEyWrKHhmIu"
 
 while true;  do
-chamber_temp=$(python /home/pw/PrusaWorkstation/ read_temp.py)
+chamber_temp=$(python /home/pw/PrusaWorkstation/read_temp.py)
 fswebcam -d /dev/video0 -r 1280x960 --top-banner --info "Pi Temp: $temp" --title "Chamber Temp: $chamber_temp" --text-colour \#fa6831 --banner-colour \#3D333F50 --line-colour \#fa6831 --rotate 270 --set contrast=20% --set saturation=35% --set  brightness=20% image1.jpg
 
 curl -T $name 'https://connect.prusa3d.com/c/snapshot' --header "token: ${token}" --header "fingerprint: ${fingerprint}"
